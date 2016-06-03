acb8cfd034b49083fa6ea56d141edaf2fc789f55
Miriam Mae Iglesia <miriamiglesia@gmail.com>
2016-06-03 15:43:44 +0800

    feature: add validate function

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 var validations = {
 	'authentication' : require( './schemas/authentication' ),
-	'user'           : require( './schemas/user' )
+	'user'           : require( './schemas/user' ),
+	'validate'       : require( './lib/validate' )
 };
 
 module.exports = validations;
