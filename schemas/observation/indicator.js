72580135a7a636faf1f21093df5e082765491edf
Josh Jeppson <josh.jeppson@schoolimprovement.com>
2017-01-18 14:38:12 -0700

    feature: add isRequired to indicator schema

--- schemas/observation/indicator.js
+++ schemas/observation/indicator.js
@@ -50,6 +50,7 @@ module.exports = function ( level ) {
 		'notApplicable' : ( setNotApplicable() ),
 		'value'         : Joi.number().optional().description( 'The point value for indicator' ),
 		'score'         : ( setScore() ),
+		'isRequired'    : Joi.boolean().default( false ).description( 'Whether the indicator response is required for observation completion' ),
 
 		'prescribedResources' : Joi.array().optional()
 			.items( Joi.string().guid().required().description( 'The observation uuid' ) )
