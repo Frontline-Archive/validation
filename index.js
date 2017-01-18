0e4f1d167e1b62b715fd313a87e0cab95a6519b5
Josh Jeppson <josh.jeppson@schoolimprovement.com>
2017-01-18 10:53:34 -0700

    add observation to index

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 var validations = {
 	'authentication' : require( './schemas/authentication' ),
-	'user'           : require( './schemas/user' )
+	'user'           : require( './schemas/user' ),
+	'observation'    : require( './schemas/observation' )
 };
 
 module.exports = validations;
