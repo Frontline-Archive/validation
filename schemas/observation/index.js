efc23e32fc2a9ff394526a3feee8e21d4f43f4f0
Josh Jeppson <josh.jeppson@schoolimprovement.com>
2017-01-18 13:08:20 -0700

    fix: match file name

--- schemas/observation/index.js
+++ schemas/observation/index.js
@@ -2,20 +2,20 @@
 
 var indicator                 = require( './indicator' );
 var masterEvidence            = require( './master-evidence' );
-var masterPrescribedResources = require( './master-prescribed-resources' );
+var masterPrescribedResources = require( './master-prescribed-resource' );
 var observation               = require( './observation' );
 var rubric                    = require( './rubric' );
 var template                  = require( './template' );
 var templateAttachment        = require( './template-attachment' );
 
 var validations = {
-	'indicator'                   : indicator,
-	'master-evidence'             : masterEvidence,
-	'master-prescribed-resources' : masterPrescribedResources,
-	'observation'                 : observation,
-	'rubric'                      : rubric,
-	'template'                    : template,
-	'template-attachment'         : templateAttachment
+	'indicator'                  : indicator,
+	'master-evidence'            : masterEvidence,
+	'master-prescribed-resource' : masterPrescribedResource,
+	'observation'                : observation,
+	'rubric'                     : rubric,
+	'template'                   : template,
+	'template-attachment'        : templateAttachment
 };
 
 module.exports = validations;
