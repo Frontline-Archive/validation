41e19e8369ee2a85428e3a09a817807a728f0447
Matt Young <matt.young@schoolimprovement.com>
2017-03-24 14:15:27 -0600

    feature: remove completedAt requirement

--- schemas/mc/evidence-completion.js
+++ schemas/mc/evidence-completion.js
@@ -7,5 +7,5 @@ module.exports = {
 	'evidenceId'     : Joi.string().guid().required().description( 'The microcredentials id' ),
 	'userId'         : Joi.string().required().description( 'The user id completing the evidence' ),
 	'completionData' : Joi.object().required().description( 'The data completion' ),
-	'completedAt'    : Joi.date().required().description( 'The complete date' )
+	'completedAt'    : Joi.date().optional().description( 'The complete date' )
 };
