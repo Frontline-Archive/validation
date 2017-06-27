6987af94e9dca5d5c4965d9d89c63237d2e17190
Daniel Arbiol <arbiol.daniel0727@gmail.com>
2017-06-27 15:38:18 +0800

    feature: add personnel id for observer/observee/reviewers
    - add reviewers attribute

--- schemas/observation/digital-signature.js
+++ schemas/observation/digital-signature.js
@@ -3,6 +3,7 @@
 var Joi = require( 'joi' );
 
 var signatureInfo = {
+	'id'      : Joi.number().integer().required().description( 'personel id of the one signing' ),
 	'sign'    : Joi.string().required().description( 'string that the user inputs for digital signatures' ),
 	'date'    : Joi.date().timestamp().raw().required().description( 'The date of which the user signed the observation' ),
 	'isValid' : Joi.boolean().required().description( 'flag that determines if the signature is valid or not' )
@@ -19,6 +20,15 @@ module.exports = {
 		.description( 'digital signature info for observer' ),
 
 	'observee' : Joi
+		.array()
+		.optional()
+		.items( Joi
+			.object()
+			.optional()
+			.keys( signatureInfo ) )
+		.description( 'digital signature info for observee' ),
+
+	'reviewers' : Joi
 		.array()
 		.optional()
 		.items( Joi
