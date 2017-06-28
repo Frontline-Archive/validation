f2c7e25378b3634d0950182ed3b4af295042cf31
Daniel Arbiol <arbiol.daniel0727@gmail.com>
2017-06-28 09:39:39 +0800

    feature: add uuid field for every signature entry

--- schemas/observation/digital-signature.js
+++ schemas/observation/digital-signature.js
@@ -3,7 +3,8 @@
 var Joi = require( 'joi' );
 
 var signatureInfo = {
-	'id'      : Joi.number().integer().required().description( 'personel id of the one signing' ),
+	'id'      : Joi.string().guid().optional().description( 'unique id for each signature added' ),
+	'userId'  : Joi.number().integer().required().description( 'personnel id of the one signing' ),
 	'sign'    : Joi.string().required().description( 'string that the user inputs for digital signatures' ),
 	'date'    : Joi.date().timestamp().raw().required().description( 'The date of which the user signed the observation' ),
 	'isValid' : Joi.boolean().required().description( 'flag that determines if the signature is valid or not' )
