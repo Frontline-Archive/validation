ab68b96d61f1fea6785725b7b6ae72d86f3d8add
Dedito Cantila <dedito.cantila@gmail.com>
2017-09-05 09:56:22 +0800

    feature: add pdOwner attribute for pd objects
    - this is for reviewers' digital signature pd identifier

--- schemas/observation/master-prescribed-resource.js
+++ schemas/observation/master-prescribed-resource.js
@@ -11,5 +11,6 @@ module.exports = {
 	'segmentLengthInSeconds' : Joi.forbidden(),
 	'dueDate'                : Joi.date().optional().description( 'The due date of observed to complete assigned resource' ),
 	'completedDate'          : Joi.date().optional().description( 'The completed date' ),
-	'assignedDate'           : Joi.date().optional().description( 'The date when the pd was assigned' )
+	'assignedDate'           : Joi.date().optional().description( 'The date when the pd was assigned' ),
+	'pdOwner'                : Joi.number().integer().optional().description( 'Owner of the pd' )
 };
