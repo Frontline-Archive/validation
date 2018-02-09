1d6ee350f2411a0da3fe719e54957335aafec329
Dedito Cantila <dedito.cantila@gmail.com>
2018-02-09 14:06:59 +0800

    fix: add issuer to mc completion
    FRL-64

--- schemas/mc/evidence-completion.js
+++ schemas/mc/evidence-completion.js
@@ -7,5 +7,6 @@ module.exports = {
 	'evidenceId'     : Joi.string().guid().required().description( 'The microcredentials id' ),
 	'userId'         : Joi.string().required().description( 'The user id completing the evidence' ),
 	'completionData' : Joi.object().required().description( 'The data completion' ),
-	'completedAt'    : Joi.date().optional().description( 'The complete date' )
+	'completedAt'    : Joi.date().optional().description( 'The complete date' ),
+	'issuer'         : Joi.string().optional().description( 'The current location of the assessor' )
 };
