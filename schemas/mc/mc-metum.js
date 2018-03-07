695a1b38d7771d28763fc953f384dc902e20fe86
Dedito Cantila <dedito.cantila@gmail.com>
2018-03-07 10:01:38 +0800

    refactor: update mc meta validation

--- schemas/mc/mc-metum.js
+++ schemas/mc/mc-metum.js
@@ -5,7 +5,7 @@ var Joi = require( 'joi' );
 module.exports = {
 	'id'                   : Joi.string().guid().optional().description( 'The microcredentials-metum id' ),
 	'mcId'                 : Joi.string().guid().required().description( 'The microcredentials id' ),
-	'overallPurpose'       : Joi.string().required().description( 'The purpose of the microcredentials' ),
+	'overallPurpose'       : Joi.string().required().allow( [ null, '' ] ).description( 'The purpose of the microcredentials' ),
 	'badgeFilename'        : Joi.string().required().description( 'The microcredentials\'s badge location' ),
-	'requiredCompetencies' : Joi.string().required().description( 'The requirements to complete the mc' )
+	'requiredCompetencies' : Joi.string().required().allow( [ null, '' ] ).description( 'The requirements to complete the mc' )
 };
